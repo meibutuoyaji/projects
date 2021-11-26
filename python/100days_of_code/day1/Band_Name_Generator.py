66ebe16fd6d1242a8a38405ffacf36c79c840f3a
Takahiro Oda <toda@nuskin.com>
2021-11-27 07:32:35 +0900

    day1

---
+++ python/100days_of_code/day1/Band_Name_Generator.py
@@ -0,0 +1,11 @@
+#create a greeting
+print("Hi there!")
+
+#Ask the user for the city that they grew up in.
+city = input("Where did you grow up?\n")
+
+#Ask the user for the name of a pet.
+pet = input("What is your pet's name?\n")
+
+#Combine the name of their city and pet and show them their band name.
+print(city + pet)
--- python/100days_of_code/day1/main.py
+++ python/100days_of_code/day1/main.py
@@ -1,1 +1,31 @@
-#this is my first python code
+#print function
+print("This is my first python code!!")
+
+#using single quotation
+print('what to print')
+
+#using \n for separating lines with backslash
+print("This is first line!\nThis is second line!")
+
+# string concatenation
+print("This" + " " + "is" + " " + " test")
+
+#use single quotation to print sentense with double quotation
+print('String Concatenation is done with the "+" sign')
+
+#input function
+print("I am " + input("what is your name?"))
+
+#print the length of input
+print(len(input("What is your name?")))
+
+#switch variable values
+a = input("a: ")
+b = input("b: ")
+
+temp = a
+a = b
+b = temp
+
+print("a: " + a)
+print("b: " + b)
