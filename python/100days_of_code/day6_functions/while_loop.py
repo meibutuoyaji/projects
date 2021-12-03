f5d07b178e40065e2a19ce2c2342ea603d91eeaa
Takahiro Oda <toda@nuskin.com>
2021-12-04 07:34:11 +0900

    function

---
+++ python/100days_of_code/day6_functions/while_loop.py
@@ -0,0 +1,13 @@
+For
+
+for item in list_of_items:
+    #Do something to each item
+
+for number in range(a,b):
+    print(number)
+
+while something_is_true:
+    #do something until it becomes false
+
+while not at_goal():
+    jump()
